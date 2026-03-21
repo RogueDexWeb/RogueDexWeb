3c1d9b007e6c7652fdb528711291224da207716c
Jimmybald1 <147992650+IBBCalc@users.noreply.github.com>
2026-03-21 11:46:15 +0100

    github pages

--- RogueDexWeb/Program.cs
+++ RogueDexWeb/Program.cs
@@ -9,7 +9,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+#if RELEASE
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://roguedexweb.github.io/RogueDexWeb/") });
+#else
+    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+#endif
+
 builder.Services.AddScoped<ILocalStorageHelper, LocalStorageHelper>();
 
 builder.Services.AddSubtleCrypto();
